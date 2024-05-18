fdfe5917f0a26da72b876ca0427ccc32ff6dc001
Muna-Elmetwally <141143414+Muna-Elmetwally@users.noreply.github.com>
2024-05-18 03:48:57 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
-from flask import Flask, render_template
+""" 7. Start flask service that does something. """
 
-from models import storage
-from models.state import State
+from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
-'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    '''The states_list page.'''
-    all_states = list(storage.all(State).values())
-    all_states.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('7-states_list.html', **ctxt)
-
-
-@app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
-    storage.close()
+#@app.route()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
